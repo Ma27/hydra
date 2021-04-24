6a7809857484a603d52d7e7aadff16151668e336
Maximilian Bosch <maximilian@mbosch.me>
2021-04-24 23:33:07 +0200

    Always return 404 for private projects
    To make "discoverability" of these harder:
    
    * scmdiff exits with `die()` in this case just as it would be the case
      if the two revisions were just invalid.
    * Web UI always says "Project not found"
    * Nix Helper doesn't seem to have a 404 behavior, so it just says
      "Resource not found" now.

--- src/lib/Hydra/Helper/Nix.pm
+++ src/lib/Hydra/Helper/Nix.pm
@@ -158,7 +158,7 @@ sub findLog {
                 {join => {build => "project"}}
             );
             if ($existsForGuest == 0) {
-                forceLogin($c);
+                notFound($c, "Resource not found");
             }
         }
 
