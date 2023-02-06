e0e3ca3822dd20b0f24685c3e3019233cf6ed9b7
Maximilian Bosch <maximilian@mbosch.me>
2023-02-06 13:48:28 +0100

    Fix nix tracking branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-based continuous build system";
 
   inputs.nixpkgs.follows = "nix/nixpkgs";
-  inputs.nix.url = "github:ma27/nix/2.13.2-fix-initNix-in-perl-bindings";
+  inputs.nix.url = "github:nixos/nix/2.13-maintenance";
 
   outputs = { self, nixpkgs, nix }:
     let
